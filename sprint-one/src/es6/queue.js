c7330e463cd2bbc719870a003e355b40dfdf562d
Ricardo <rroque98@gmail.com>
2018-06-13 23:48:41 -0700

    Added solution to queue.js under es6 pattern.

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -1,7 +1,23 @@
 class Queue {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
   constructor() {
+    this.count = 0;
+    this.nextKeyInLine = 0;
+    this.storage = {};
   }
 
+  enqueue(value) {
+    this.count++;
+    this.storage[this.count] = value;
+  }
+
+  dequeue() {
+    this.nextKeyInLine++;
+    var temp = this.storage[this.nextKeyInLine];
+    delete this.storage[this.nextKeyInLine];
+    return temp;
+  }
+
+  size() {
+    return Object.keys(this.storage).length;
+  }
 }
