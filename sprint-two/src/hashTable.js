7f299b24d3be97a46aa132a27649114c4b1ea71f
rroque98 <rroque98@gmail.com>
2018-06-16 18:11:10 -0700

    Finishes data structures bare mins

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,14 +7,39 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(index);
+
+  if (!bucket) {
+    this._storage.set(index, [[k, v]]);
+  } else {
+    for (let i = 0; i < bucket.length; i++) {
+      if (k === bucket[i][0]){
+        bucket[i][1] = v;
+      } else {
+        bucket.push([k, v]);
+      }
+    }
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  debugger;
+  for (var i = 0; i < this._storage.get(index).length; i++) {
+    if (k === this._storage.get(index)[i][0]) {
+      return this._storage.get(index)[i][1];
+    }
+  }
 };
 
-HashTable.prototype.remove = function(k) {
+HashTable.prototype.remove = function(k) { 
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(index);
+  for (var i = 0; i < bucket.length; i++) {
+    if (k === bucket[i][0]){
+      bucket[i][1] = undefined;
+    }
+  }
 };
 
 
